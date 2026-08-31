84b12224a39433e83d68c2a1498b27f32ca7be83
DK <vopros-otvet1990@mail.ru>
2026-08-31 14:58:01 +0300

    Init project

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "company-finance-management-system"
---
+++ src/main/java/com/example/company_finance_management_system/CompanyFinanceManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.company_finance_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyFinanceManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyFinanceManagementSystemApplication.class, args);
+	}
+
+}
