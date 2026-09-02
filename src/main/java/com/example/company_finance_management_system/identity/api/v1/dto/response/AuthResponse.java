75ebe9cbce515c701c65f941aa287eb317b85a02
DK <vopros-otvet1990@mail.ru>
2026-09-02 13:33:18 +0300

    Revert "fix: deleted .env files from git"
    This reverts commit d3fca2915d6928214c1005366808171e79b64405.

--- src/main/java/com/example/company_finance_management_system/identity/api/v1/dto/response/AuthResponse.java
+++
@@ -1,9 +0,0 @@
-package com.example.company_finance_management_system.identity.api.v1.dto.response;
-
-public record AuthResponse(
-
-        String accessToken,
-        String refreshToken
-
-) {
-}
--- src/main/java/com/example/company_finance_management_system/identity/repository/SessionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.company_finance_management_system.identity.repository;
-
-import com.example.company_finance_management_system.identity.entity.Session;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SessionRepository extends JpaRepository<Session, Long> {
-
-}
